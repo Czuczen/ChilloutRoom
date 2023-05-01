1652837de4f6537a1d6052b95889b16aeddec07d
Czuczen <czuczengames@gmail.com>
2023-05-01 11:20:09 +0200

    Komentowanie klas TicTacToeClientData, TicTacToeEndMatchData i TicTacToeStartMatchData dla projektu CzuczenLand.Application

--- src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/TicTacToe/Dto/TicTacToeClientData.cs
+++ src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/TicTacToe/Dto/TicTacToeClientData.cs
@@ -1,20 +1,47 @@
 ﻿namespace CzuczenLand.ExtendingFunctionalities.SignalRHubs.TicTacToe.Dto;
 
+/// <summary>
+/// Klasa reprezentująca dane klienta gry w kółko i krzyżyk.
+/// </summary>
 public class TicTacToeClientData
 {
+    /// <summary>
+    /// Określa, czy klient chce zagrać z przeciwnikiem.
+    /// </summary>
     public bool WantToPlayWithOpponent { get; set; }
         
+    /// <summary>
+    /// Określa, czy klient oczekuje na przeciwnika.
+    /// </summary>
     public bool WaitForOpponent { get; set; }
         
+    /// <summary>
+    /// Nazwa grupy, do której należy klient.
+    /// </summary>
     public string GroupName { get; set; }
         
+    /// <summary>
+    /// Symbol, którym gra klient.
+    /// </summary>
     public string Symbol { get; set; }
 
+    /// <summary>
+    /// Identyfikator połączenia klienta.
+    /// </summary>
     public string ConnectionId { get; set; }
         
+    /// <summary>
+    /// Identyfikator użytkownika przeciwnika.
+    /// </summary>
     public long? OpponentUserId { get; set; }
         
+    /// <summary>
+    /// Identyfikator połączenia przeciwnika.
+    /// </summary>
     public string OpponentConnectionId { get; set; }
         
+    /// <summary>
+    /// Określa, czy gra jest w toku.
+    /// </summary>
     public bool GameInProgress { get; set; }
 }
--- src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/TicTacToe/Dto/TicTacToeEndMatchData.cs
+++ src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/TicTacToe/Dto/TicTacToeEndMatchData.cs
@@ -3,14 +3,29 @@ using CzuczenLand.ExtendingModels.Models.Others;
 
 namespace CzuczenLand.ExtendingFunctionalities.SignalRHubs.TicTacToe.Dto;
 
+/// <summary>
+/// Klasa reprezentująca dane końcowe rozgrywki w kółko i krzyżyk
+/// </summary>
 [AutoMapFrom(typeof(TicTacToeStorage))]
 public class TicTacToeEndMatchData
 {
+    /// <summary>
+    /// Liczba rozegranych gier
+    /// </summary>
     public int GamesPlayed { get; set; }
         
+    /// <summary>
+    /// Liczba wygranych gier
+    /// </summary>
     public int GamesWon { get; set; }
         
+    /// <summary>
+    /// Liczba przegranych gier
+    /// </summary>
     public int GamesLost { get; set; }
         
+    /// <summary>
+    /// Liczba zremisowanych gier
+    /// </summary>
     public int TiedGames { get; set; }
 }
