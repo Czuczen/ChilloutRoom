f3c55da55eb7c24ca6d3f087dc64e4747aa6bc54
Czuczen <czuczengames@gmail.com>
2023-05-02 14:49:43 +0200

    Komentowanie klas StorageHubHelper, StorageHub i ShopTransaction dla projektu CzuczenLand.Application

--- src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/Plantation/Storage/StorageHub.cs
+++ src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/Plantation/Storage/StorageHub.cs
@@ -19,29 +19,95 @@ using Microsoft.AspNet.SignalR;
 
 namespace CzuczenLand.ExtendingFunctionalities.SignalRHubs.Plantation.Storage;
 
+/// <summary>
+/// Hub SignalR obsługujący magazyn/zasoby plantacji
+/// </summary>
 [AbpMvcAuthorize]
 public class StorageHub : Hub, ITransientDependency
 {
+    /// <summary>
+    /// Repozytorium przechowujące susz w bazie danych.
+    /// </summary>
     private readonly IRepository<DriedFruit> _driedFruitRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące lampy w bazie danych.
+    /// </summary>
     private readonly IRepository<Lamp> _lampRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące nawozy w bazie danych.
+    /// </summary>
     private readonly IRepository<Manure> _manureRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące gleby w bazie danych.
+    /// </summary>
     private readonly IRepository<Soil> _soilRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące wodę w bazie danych.
+    /// </summary>
     private readonly IRepository<Water> _waterRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące doniczki w bazie danych.
+    /// </summary>
     private readonly IRepository<Pot> _potRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące nasiona w bazie danych.
+    /// </summary>
     private readonly IRepository<Seed> _seedRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące bonusy w bazie danych.
+    /// </summary>
     private readonly IRepository<ExtendingModels.Models.Products.Bonus> _bonusRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące dzielnice w bazie danych.
+    /// </summary>
     private readonly IRepository<District> _districtRepository;
+    
+    /// <summary>
+    /// Repozytorium przechowujące magazyny plantacji w bazie danych.
+    /// </summary>
     private readonly IPlantationStorageService _plantationStorageService;
+    
+    /// <summary>
+    /// Repozytorium przechowujące magazyny gracza w bazie danych.
+    /// </summary>
     private readonly IPlayerStorageService _playerStorageService;
 
+    
     /// <summary>
-    /// Musi być public i musi mieć setter
+    /// Właściwość pozwalająca na uzyskanie dostępu do sesji Abp, która przechowuje informacje dotyczące aktualnie zalogowanego użytkownika.
+    /// Właściwość musi być public oraz mieć getter i setter dla poprawnego działania wstrzykiwania właściwości.
     /// </summary>
     public IAbpSession AbpSession { get; set; }
     
+    /// <summary>
+    /// Interfejs ILogger służy do rejestrowania komunikatów z aplikacji.
+    /// Właściwość musi być public oraz mieć getter i setter dla poprawnego działania wstrzykiwania właściwości.
+    /// </summary>
     public ILogger Logger { get; set; }
     
     
+    /// <summary>
+    /// Konstruktor, który ustawia wstrzykiwane zależności.
+    /// </summary>
+    /// <param name="driedFruitRepository">Repozytorium suszu.</param>
+    /// <param name="lampRepository">Repozytorium lamp.</param>
+    /// <param name="manureRepository">Repozytorium nawozów.</param>
+    /// <param name="soilRepository">Repozytorium gleb.</param>
+    /// <param name="waterRepository">Repozytorium wody.</param>
+    /// <param name="potRepository">Repozytorium donic.</param>
+    /// <param name="seedRepository">Repozytorium nasion.</param>
+    /// <param name="bonusRepository">Repozytorium bonusów.</param>
+    /// <param name="districtRepository">Repozytorium dzielnic.</param>
+    /// <param name="plantationStorageService">Serwis magazynu plantacji.</param>
+    /// <param name="playerStorageService">Serwis magazynu gracza.</param>
     public StorageHub(
         IRepository<DriedFruit> driedFruitRepository,
         IRepository<Lamp> lampRepository,
@@ -72,6 +138,14 @@ public class StorageHub : Hub, ITransientDependency
         _playerStorageService = playerStorageService;
     }
 
+    /// <summary>
+    /// Metoda służąca do przeprowadzania transakcji zasobów w sklepie.
+    /// </summary>
+    /// <param name="productId">Identyfikator produktu.</param>
+    /// <param name="entity">Nazwa encji.</param>
+    /// <param name="howMany">Ilość przetwarzanych zasobów.</param>
+    /// <param name="action">Typ akcji (np. "buy" lub "sell").</param>
+    /// <returns></returns>
     [UnitOfWork]
     public virtual async Task<ShopTransaction> ResourcesTransaction(int productId, string entity, decimal howMany, string action)
     {
@@ -98,6 +172,15 @@ public class StorageHub : Hub, ITransientDependency
         }
     }
 
+    /// <summary>
+    /// Metoda służąca do przeprowadzania transakcji produktu w sklepie.
+    /// </summary>
+    /// <param name="playerPlantationStorage">Magazyn plantacji gracza.</param>
+    /// <param name="action">Typ akcji (kupna/sprzedaży).</param>
+    /// <param name="amount">Ilość produktu.</param>
+    /// <param name="shopTransaction">Obiekt transakcji sklepu.</param>
+    /// <param name="productId">Identyfikator produktu.</param>
+    /// <param name="entity">Nazwa encji.</param>
     private async Task ProcessProduct(PlantationStorage playerPlantationStorage, string action, decimal amount, 
         ShopTransaction shopTransaction, int productId, string entity)
     {
