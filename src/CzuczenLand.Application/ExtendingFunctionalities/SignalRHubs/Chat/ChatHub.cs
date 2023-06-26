d73cc346a9dbab7fba40919e7007884a62a1667a
Czuczen <czuczengames@gmail.com>
2023-06-26 13:12:17 +0200

    Komentowanie klas ChatHub i ChatMessageData dla projektu CzuczenLand.Application

--- src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/Chat/ChatHub.cs
+++ src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/Chat/ChatHub.cs
@@ -9,26 +9,38 @@ using Microsoft.AspNet.SignalR;
 namespace CzuczenLand.ExtendingFunctionalities.SignalRHubs.Chat;
 
 /// <summary>
-/// [UnitOfWork] - Jednostka pracy wymaga ITransientDependency
-/// IOnlineClientManager - wymaga ITransientDependency
-/// AbpSession i Logger - wymaga ITransientDependency
+/// Hub SignalR obsługujący czat.
 /// [AbpAuthorize] dawało wyjątek - Abp.Authorization.AbpAuthorizationException: Current user did not login to the application!
 /// Nic nie psuł ale jednak wyskakiwał. W każdym hub'ie wyskakiwał. Użycie [AbpMvcAuthorize] rozwiązało problem.
 /// </summary>
 [AbpMvcAuthorize]
 public class ChatHub : Hub, ITransientDependency
 {
+    /// <summary>
+    /// Właściwość pozwalająca na uzyskanie dostępu do sesji Abp, która przechowuje informacje dotyczące aktualnie zalogowanego użytkownika.
+    /// Właściwość musi być public oraz mieć getter i setter dla poprawnego działania wstrzykiwania właściwości.
+    /// </summary>
     public IAbpSession AbpSession { get; set; }
         
+    /// <summary>
+    /// Interfejs ILogger służy do rejestrowania komunikatów z aplikacji.
+    /// Właściwość musi być public oraz mieć getter i setter dla poprawnego działania wstrzykiwania właściwości.
+    /// </summary>
     public ILogger Logger { get; set; }
 
-    
+    /// <summary>
+    /// Konstruktor, który ustawia wstrzykiwane zależności.
+    /// </summary>
     public ChatHub()
     {
         AbpSession = NullAbpSession.Instance;
         Logger = NullLogger.Instance;
     }
 
+    /// <summary>
+    /// Metoda wysyłająca wiadomość do wszystkich klientów.
+    /// </summary>
+    /// <param name="message">Treść wiadomości.</param>
     public void SendMessageToAll(string message)
     {
         try
--- src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/Chat/Dto/ChatMessageData.cs
+++ src/CzuczenLand.Application/ExtendingFunctionalities/SignalRHubs/Chat/Dto/ChatMessageData.cs
@@ -1,8 +1,17 @@
 ﻿namespace CzuczenLand.ExtendingFunctionalities.SignalRHubs.Chat.Dto;
 
+/// <summary>
+/// Klasa przechowująca dane wiadomości czatu.
+/// </summary>
 public class ChatMessageData
 {
+    /// <summary>
+    /// Nazwa użytkownika, który wysłał wiadomość.
+    /// </summary>
     public string UserName { get; set; }
         
+    /// <summary>
+    /// Treść wiadomości.
+    /// </summary>
     public string Message { get; set; }
 }
